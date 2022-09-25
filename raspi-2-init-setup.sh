b13ce63e7642e2bb6e84a7580aef10af28cc1847
Jeremy Kirscht <jeremykirscht@gmail.com>
2022-09-24 21:07:40 -0500

    Create file
    cause

---
+++ raspi-2-init-setup.sh
@@ -0,0 +1,28 @@
+# Update 
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+# Install dev stuff
+sudo apt-get install build-essential libncurses-dev bison flex libssl-dev libelf-dev
+sudo apt-get install vim
+sudo apt-get install htop
+
+# Install Security packages 
+sudo apt-get install fail2ban
+
+# Install Go
+sudo apt-get install golang
+
+
+# Disable Swapfile
+sudo dphys-swapfile swapoff 
+sudo dphys-swapfile uninstall
+sudo update-rc.d dphys-swapfile remove
+sudo swapon --summary
+
+# Install ZRAM
+sudo apt install zram-tools
+
+# Install Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
